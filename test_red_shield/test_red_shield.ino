ead6bf94f582901eb1644e1f7bfd3c31b95233f0
jozles <pinkasfeld@combox.fr>
2020-11-17 22:57:17 +0100

    ajout DS3231

--- test_red_shield/test_red_shield.ino
+++ test_red_shield/test_red_shield.ino
@@ -1,5 +1,7 @@
 #include <SPI.h>
 #include <SD.h>
+#include <Wire.h>     //biblio I2C pour RTC 3231
+#include "ds3231.h"
 
 #define SDC 3
 #define LED 2
@@ -13,30 +15,70 @@ uint8_t sdc=SDC;
 uint32_t dur=DUR;
 uint32_t per=PER;
 
+Ds3231 pendule;
+
+#define DS3231_I2C_ADDRESS 0x68;
+byte ss,mm,hh,wd,dd,dm,yy;
+
+#define PINVCCDS 19   // alim + DS3231
+#define PINGNDDS 18   // alim - DS3231
+
 void sdInit();
 void sdFileRead(char* fileName);
+void printdh();
 
 void setup() {
   digitalWrite(LED,LOW);
   pinMode(LED,OUTPUT);
   delay(100);
   Serial.begin(115200);
-  Serial.println("\nW watchdog ; S SDcard");
+  Serial.println("\nTest red shield\nW watchdog ; S SDcard ; D DS3231 clock ; O mise à l'heure");
   test=getch();Serial.println(test);
 
     switch (test){
+      
       case '\W':
         Serial.println("- double la période ; + la divise par 2 ; 1,2,5,0 ajoute 100,200,500,1000 ");
         Serial.println(" > add 1mS à la durée < sub 1 mS ; $ menu");
         break;
+        
       case '\S':
         Serial.print("CS pin (");Serial.print(sdc);Serial.println(")");
 //        sdc=(uint8_t)getch()-48;Serial.println(sdc);
         dur=1;
         sdInit();
         sdFileRead(testFile);
+        break;
+        
+      case '\D':
+        Wire.begin();delay(100);
+        digitalWrite(PINGNDDS,LOW);pinMode(PINGNDDS,OUTPUT);  
+        digitalWrite(PINVCCDS,HIGH);pinMode(PINVCCDS,OUTPUT);
+        pendule.i2cAddr=DS3231_I2C_ADDRESS;
+        
+        pendule.readTime(&ss,&mm,&hh,&wd,&dd,&dm,&yy);
+        printdh();
+        break;
+
+      case '\O':
+Serial.println("*** modifier date/heure puis relancer ***");
+
+        Wire.begin();delay(100);
+        digitalWrite(PINGNDDS,LOW);pinMode(PINGNDDS,OUTPUT);  
+        digitalWrite(PINVCCDS,HIGH);pinMode(PINVCCDS,OUTPUT);
+        pendule.i2cAddr=DS3231_I2C_ADDRESS;
+/*
+        ss=0;mm=45;hh=14;wd=4;dd=28;dm=10;yy=20;
+        pendule.setTime(ss,mm,hh,wd,dd,dm,yy);
+*/
+        pendule.readTime(&ss,&mm,&hh,&wd,&dd,&dm,&yy);
+        printdh();
+        break;
+                        
+      default:
+        break;
     }
- 
+  Serial.println("end setup");
 }
 
 void loop() {
@@ -60,7 +102,7 @@ void loop() {
       Serial.print(in);Serial.print(" per=");Serial.print(per);Serial.print(" dur=");Serial.println(dur);
     }
   }
-  if(test==83){             /* test SDcard retour menu */    
+  if(test==83 || test=='\O' || test=='\D'){             /* test SDcard ou DS3231 retour menu */    
     while(1){};
   }
   if(in=='\$'){           /* retour menu */    
@@ -110,4 +152,16 @@ char getch()
 {
   while(Serial.available()==0){led();delay(1000);}
   return Serial.read();
+}
+
+void printdh0(byte v)
+{
+  if(v<10){Serial.print("0");}Serial.print(v);
+}
+
+void printdh()
+{
+  printdh0(dd);Serial.print("/");printdh0(dm);Serial.print("/");printdh0(yy);Serial.print(" ");
+  printdh0(hh);Serial.print(":");printdh0(mm);Serial.print(":");printdh0(ss);
+  Serial.println();
 }
